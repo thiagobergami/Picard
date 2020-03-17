a24cfc3d3006e2e3c64edd3bad011e86a988ee87
thiagobergami <thiagobergamig@gmail.com>
2020-03-17 18:50:28 -0300

    Finalizando primeiro layout base

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,38 +10,251 @@
 
 @section('content')
 <div class="doc-content col-md-9 col-12 order-1">
-  <div class="content-inner">
-    
-  </div>
-</div>    
+    <div class="content-inner">
+        <section id="intro">
+            <h2 class="titulo-doc-h2 " style="margin-top: 5px;">Picard</h2>
+            <hr>
+            <p class="pg">Picard (Plataforma de interação, consulta e acesso à rede de Dados) é um sistema Taesa Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus, animi aliquid eius quia ratione laborum magni corrupti veniam unde explicabo quo incidunt deserunt sapiente ipsa aperiam nisi deleniti adipisci!</p>
+
+        </section>
+        <br>
+        <section id="como-utilizar">
+            <h2 class="titulo-doc-h2 " style="margin-top: 5px;">Como Utilizar?</h2>
+            <hr>
+            <p class="pg">A Plataforma oferece acesso à base de dados voltadas para análise Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus, animi aliquid eius quia ratione laborum magni corrupti veniam unde explicabo quo incidunt deserunt sapiente ipsa aperiam nisi deleniti adipisci!</p>
+        </section>
+        <!-- Foreach para gerar essa section -->
+        <section id="gestao-ativo">
+            <h3 class="titulo-doc" id="gestao-ativo">Gestão de Ativos</h3>
+            <div class="mt-3 mb-5">
+                <div class="accordion accordion-taesa text-texto-principal-2" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header p-0" id="vida">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-left c1" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <span class="letter">Vida útil dos Ativos.</span>
+                                </button>
+                            </h5>
+                        </div>
+                        <!-- Esses elementos devem ser carregados quando forem chamados -->
+                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="parcela">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c2" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                    <span class="letter">Parcela Variável.</span>
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="capex-dto">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c3" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Capex - DTO</span>
+                                </button>
+                            </h5>
+                        </div>
+
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="demandas-plr">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c4" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Demandas - PLR</span>
+                                </button>
+                            </h5>
+                        </div>
+
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="contratos">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c1" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Análise de Contratos</span>
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="engenharia">
+            <h3 class="titulo-doc" id="gestao-ativo">Engenharia de O&M</h3>
+            <div class="mt-3 mb-5">
+                <div class="accordion accordion-taesa text-texto-principal-2" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header p-0" id="telecom">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-left c1" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <span class="letter">Canais de Telecom.</span>
+                                </button>
+                            </h5>
+                        </div>
+                        <!-- Esses elementos devem ser carregados quando forem chamados -->
+                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="projetos">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c2" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                    <span class="letter">Horas em projetos - GOM.</span>
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="opex">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c3" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Realização de OPEX</span>
+                                </button>
+                            </h5>
+                        </div>
+
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="capex-gom">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c4" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">CAPEX - GOM</span>
+                                </button>
+                            </h5>
+                        </div>
+
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="linha">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c1" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Relatório de Manutenção - Linha</span>
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="oleo">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c2" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Análise de Óleo</span>
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0" id="oleo-reator">
+                            <h5 class="mb-0">
+                                <button class="btn btn-block collapsed btn-link fs-16 py-3 text-decoration-none text-left c3" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                    <span class="letter">Análise de Óleo - Reatores</span>
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="fs-16">Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
 <div class="doc-sidebar col-md-3 col-12 order-0 d-none d-md-flex">
-  <div id="doc-nav" class="doc-nav">
-    <!--Criar aqui um foreach para expor as áreas de interesse e suas análises -->
-      <nav id="doc-menu" class="nav doc-menu flex-column sticky head">
-        <a href="#" class="nav-link scrollto text-left text-menu">Gestão de Ativos</a>
-          <nav class="doc-sub-menu nav flex-column">
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Vida últil dos Ativos</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Parcela Variável</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Capex - DTO</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Demandas - PLR</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Análise de Contratos</a>
-          </nav>
-          <a href="#" class="nav-link scrollto text-left text-menu">Engenharia de O&M</a>
-          <nav class="doc-sub-menu nav flex-column">
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Canais de Telecom</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Horas em projetos - GOM</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Realização de OPEX</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Capex - GOM</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Relatório de Manutenção - Linha</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Análise de Óleo</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Análise de Óleo - Reatores</a>
-          </nav>
-          <a href="#" class="nav-link scrollto text-left text-menu">Reforços e Melhorias</a>
-          <nav class="doc-sub-menu nav flex-column">
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Execução Financeira - Projetos</a>
-            <a href="#" class="nav-link scrollto ml-5 text-menu">Execução Física - Projetos</a>
-          </nav>
-      </nav>
+    <div id="doc-nav" class="doc-nav">
+        <!--Criar aqui um foreach para expor as áreas de interesse e suas análises -->
+        <nav id="doc-menu" class="nav doc-menu flex-column sticky head">
+            <a href="#" class="nav-link active text-left text-menu">Menu</a>
+            <a href="#intro" class="nav-link scrollto text-left text-menu">Picard</a>
+            <a href="#como-utilizar" class="nav-link scrollto text-left text-menu">Como Utilizar?</a>
+            <a href="#gestao-ativo" class="nav-link scrollto text-left text-menu">Gestão de Ativos</a>
+            <nav class="doc-sub-menu nav flex-column">
+                <a href="#vida" class="nav-link scrollto ml-5 text-menu">Vida últil dos Ativos</a>
+                <a href="#parcela" class="nav-link scrollto ml-5 text-menu">Parcela Variável</a>
+                <a href="#capex-dto" class="nav-link scrollto ml-5 text-menu">Capex - DTO</a>
+                <a href="#demandas-plr" class="nav-link scrollto ml-5 text-menu">Demandas - PLR</a>
+                <a href="#contrato" class="nav-link scrollto ml-5 text-menu">Análise de Contratos</a>
+            </nav>
+            <a href="#engenharia" class="nav-link scrollto text-left text-menu">Engenharia de O&M</a>
+            <nav class="doc-sub-menu nav flex-column">
+                <a href="#telecom" class="nav-link scrollto ml-5 text-menu">Canais de Telecom</a>
+                <a href="#projetos" class="nav-link scrollto ml-5 text-menu">Horas em projetos - GOM</a>
+                <a href="#opex" class="nav-link scrollto ml-5 text-menu">Realização de OPEX</a>
+                <a href="#capex-gom" class="nav-link scrollto ml-5 text-menu">Capex - GOM</a>
+                <a href="#linha" class="nav-link scrollto ml-5 text-menu">Relatório de Manutenção - Linha</a>
+                <a href="#oleo" class="nav-link scrollto ml-5 text-menu">Análise de Óleo</a>
+                <a href="#oleo-reator" class="nav-link scrollto ml-5 text-menu">Análise de Óleo - Reatores</a>
+            </nav>
+            <a href="#" class="nav-link scrollto text-left text-menu">Reforços e Melhorias</a>
+            <nav class="doc-sub-menu nav flex-column">
+                <a href="#" class="nav-link scrollto ml-5 text-menu">Execução Financeira - Projetos</a>
+                <a href="#" class="nav-link scrollto ml-5 text-menu">Execução Física - Projetos</a>
+            </nav>
+        </nav>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="js/pickadate/lib/themes/classic.css">
     <link rel="stylesheet" href="js/pickadate/lib/themes/classic.date.css">
 </head>
-<body class = "body-green">
+<body class="body-green">
     <div class="page-wrapper">
         <header id="header" class="header">
             <div class="container">
@@ -31,62 +31,126 @@
                             <div class="align-items-center d-flex">
                                 <div class="d-column d-inline-block d-none mr-3 px-3">
                                     <a href="/">
-                                        <img alt="TAESA" src="img/logo-taesa.png" width="130px" >
+                                        <img alt="TAESA" src="img/logo-taesa.png" width="130px">
                                     </a>
                                 </div>
-                                    <h2 style="margin-top: 15px;" class="border-left border-white col-12 d-md-inline-block d-none mr-3 px-3 text-branco">Bem vindo <span class ="typed" data-typed-items="à Informação, à Inovação, à Inteligência, ao Picard."></span></h2>
+                                <h2 style="margin-top: 15px;" class="border-left border-white col-12 d-md-inline-block d-none mr-3 px-3 text-branco">Bem vindo <span class="typed" data-typed-items="à Informação, à Inovação, à Inteligência, ao Picard."></span></h2>
                             </div>
                             <div class="btn btn-azul-5 cor-azul-hover text-branco fs-16 px-4 py-2  mt-lg-2 mt-2">
                                 <span><i class="ft-edit"></i>Login</span>
                             </div>
                         </div>
                     </div>
-                </div><!--//branding-->
-            </div><!--//container-->
+                </div>
+                <!--//branding-->
+            </div>
+            <!--//container-->
         </header>
         <div class="doc-wrapper">
-                @section('sidebar')
-                    @show
-                    <div class="container">
-                        <div class="doc-body row mt-5">
-                        @yield('content')
-                        </div>
-                    </div>
-                @yield('barra-lateral')
+            @section('sidebar')
+            @show
+            <div class="container">
+                <div class="doc-body row mt-5">
+                    @yield('content')
+                </div>
+            </div>
+            @yield('barra-lateral')
         </div>
     </div>
-</div>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-
-    <!-- JS Plugin Slider -->
+    </div>
+    <!-- Main Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="assets/plugins/jquery.js"></script>
+    <!--    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!--     <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script> -->
+    <script type="text/javascript" src="assets/plugins/prism/prism.js"></script>
+    <script type="text/javascript" src="assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/stickyfill/dist/stickyfill.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    <!--<script type="text/javascript" src=" slide/slick/slick.min.js"></script>-->
+    <script type="text/javascript" src="assets/plugins/jquery-migrate.js"></script>
+    <script type="text/javascript" src="slide/slick/slick.js"></script>
+    <!-- galeria de imagens lightbox -->
+    <script src="js/magnific-popup.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.0/js/ion.rangeSlider.min.js"></script>
+    <!-- JS Plugin data -->
     <script src="js/pickadate/lib/picker.js"></script>
     <script src="js/pickadate/lib/picker.date.js"></script>
 
     <script src="js/index.js"></script>
     <script src="js/shuffle-text.js"></script>
     <script src="typed.js/typed.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" rel="stylesheet" />
     <script>
         "use strict";
         var typed_strings = $(".typed").data('typed-items');
         typed_strings = typed_strings.split(',')
         new Typed('.typed', {
-            strings: typed_strings,
-            loop: false,
-            typeSpeed: 100,
-            backSpeed: 50,
-            backDelay: 2000
+            strings: typed_strings
+            , loop: false
+            , typeSpeed: 100
+            , backSpeed: 50
+            , backDelay: 2000
         });
-        
+
+        $(document).ready(function() {
+            $('.galeria').magnificPopup({
+                delegate: 'button', // child items selector, by clicking on it popup will open
+                type: 'image'
+                , gallery: {
+                    enabled: true
+                }
+            });
+        });
+        // With JQuery ion.rangeslider
+        $(".js-range-slider").ionRangeSlider({
+            min: 0
+            , max: 100
+            , skin: "big"
+            , postfix: "%"
+        , });
+        $("#range2").ionRangeSlider({
+            skin: "flat"
+            , grid: true
+            , min: 0
+            , max: 6
+            , grid: true
+            , step: 1
+            , values: [0, 1, 2, 3, 4, 5, 6]
+        , });
+        $('.slide').slick({
+            dots: false
+            , infinite: true
+            , speed: 300
+            , slidesToShow: 4
+            , slidesToScroll: 4
+            , prevArrow: '<button class="slick-prev"><i class="ft-seta_left"></i></button>'
+            , nextArrow: '<button class="slick-next"><i class="ft-seta_right"></i></button>'
+            , responsive: [{
+                    breakpoint: 992
+                    , settings: {
+                        dots: true
+                        , slidesToShow: 2
+                        , slidesToScroll: 2
+                        , arrows: false
+                    }
+                }
+                , {
+                    breakpoint: 576
+                    , settings: {
+                        dots: true
+                        , slidesToShow: 1
+                        , slidesToScroll: 1
+                        , arrows: false
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
     </script>
 </body>
 
